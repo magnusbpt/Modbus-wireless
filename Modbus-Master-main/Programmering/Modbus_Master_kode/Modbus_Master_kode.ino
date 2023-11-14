610ef3f2995f7fcf6a2e1d25d5ec3333acacb6d7
magnusbpt <magnus.bpt@gmail.com>
2023-11-14 13:31:43 +0100

    ændring

--- Modbus-Master-main/Programmering/Modbus_Master_kode/Modbus_Master_kode.ino
+++ Modbus-Master-main/Programmering/Modbus_Master_kode/Modbus_Master_kode.ino
@@ -29,7 +29,7 @@
 unsigned char* simBuffer = (unsigned char*)malloc(1000 * sizeof(char));
 // unsigned char* setupMessage = (unsigned char*)malloc(1000 * sizeof(char));
 // unsigned char* messageBuffer = (unsigned char*)malloc(2000 * sizeof(char));
-char messageBuffer[2000];
+char messageBuffer[2500];
 // char serverMessage[500];
 unsigned char modBuffer[50];  // Buffer to store modbus slave response
 char IMEI[10];                // Array to store IMEI number
@@ -117,7 +117,6 @@ void setup() {
   modbusSerial.begin(9600);
 
   //Setup mode of pins on master
-  pinMode(DTR_E, OUTPUT);
   pinMode(PWRKEY, OUTPUT);
   pinMode(BLUE_LED, OUTPUT);
   pinMode(GREEN_LED, OUTPUT);
@@ -129,7 +128,6 @@ void setup() {
   digitalWrite(RED_LED, LOW);
 
   digitalWrite(PWRKEY, HIGH);  // Set powerkey high so sim module stay on/off
-  digitalWrite(DTR_E, LOW);    // Maybe not used?
 
   millisDelay(3000);  //Start delay
 
@@ -231,6 +229,14 @@ void loop() {
       serverRead();  //Read server message
       clrsimBuffer();
 
+      simSerial.println(F("AT+CACLOSE=0"));  // Check if connected to the correct APN
+      responseCheck("OK", 300);
+      clrsimBuffer();
+
+      simSerial.println(F("AT+CAOPEN=0,0,\"UDP\",\"164.92.164.168\",8080"));  // Open UDP connection/socket
+      responseCheck("OK", 2000);
+      clrsimBuffer();
+
       testbuf2[0] = transactionID;
       testbuf2[10] = highByte(CRC16_modbus(testbuf2, 10));
       testbuf2[11] = lowByte(CRC16_modbus(testbuf2, 10));
@@ -289,7 +295,7 @@ void loop() {
       masterData[18] = lowByte(CRC16_modbus(masterData, 17));
 
       simSerial.println(F("AT+CASEND=0,19"));  //Write to send message 19 bytes long
-      millisDelay(500);
+      millisDelay(300);
       clrsimBuffer();
 
       simSerial.write(masterData, 19);  //Send message
@@ -297,6 +303,8 @@ void loop() {
 
       simSerial.println(F("AT+CARECV=0,1460"));  //Read recieved message from server
       responseCheck("00", 2000);
+
+      state = slaveread;
       break;
   }
 
@@ -322,9 +330,24 @@ void simSetup() {
       simpow();
     }
   }
-  stop = 1;  // Set stop for next siminit call
+  start = 0;  // Start bit used in simSetup
+  stop = 1;   // Set stop for next siminit call
 
   clrsimBuffer();  // Clear sim response buffer
+
+  // while (stop) {                        // Stay in loop while stop is set
+  //   simSerial.println(F("AT+CGREG?"));  // Print AT
+  //   start = responseCheck("0,2", 1000);
+  //   if (start) {  // If start is set, stop while
+  //     stop = 0;
+  //   }
+  // }
+  // start = 0;  // Start bit used in simSetup
+  // stop = 1;   // Set stop for next siminit call
+  
+  simSerial.println(F("AT+CPIN?"));  //Check SIM card status
+  responseCheck("READY", 2000);
+  clrsimBuffer();
 
   simSerial.println(F("AT+GSN"));  // Sent at command to get IMEI number
   responseCheck("OK", 2000);
@@ -337,13 +360,29 @@ void simSetup() {
   }
   clrsimBuffer();  // Clear sim response buffer
 
+  simSerial.println(F("AT+CSQ"));  // Check signal quality
+  responseCheck("OK", 2000);
+  for (int i = 0; i < 2; i++) {  // Put response into IMEI array
+    CSQ[i] = simBuffer[i + 7];
+  }
+  clrsimBuffer();  // Clear sim response buffer
 
   simSerial.println(F("AT+CGATT?"));  // Check PS Service
   responseCheck("1", 2000);
   clrsimBuffer();
 
+  simSerial.println(F("AT+COPS?"));  // Check operator. See operator e.g Telia and if connected to NB or CAT-M
+  if (responseCheck("9", 2000) != 1) {
+    responseCheck("7", 2000);
+  }
+  clrsimBuffer();
+
+  simSerial.println(F("AT+CGNAPN"));  // Check if connected to the correct APN
+  responseCheck("iot.1nce.net", 2000);
+  clrsimBuffer();
+
   simSerial.println(F("AT+CNCFG=0,1,\"iot.1nce.net\""));  // Check if connected to the correct APN
-  if (responseCheck("OK", 2000) != 1) {                  //If not connected
+  if (responseCheck("OK", 2000) != 1) {                   //If not connected
     simSerial.println(F("AT+CFUN=0"));                    // Set phone functionality to minimal
     responseCheck("OK", 2000);
     clrsimBuffer();
@@ -355,29 +394,12 @@ void simSetup() {
     simSerial.println(F("AT+CFUN=1"));  // Set phone funtionality to full
     responseCheck("OK", 2000);
     clrsimBuffer();
-  
+
     simSerial.println(F("AT+COPS=1,2,\"23820\""));  // Set phone funtionality to full
     responseCheck("OK", 2000);
     clrsimBuffer();
   }
   clrsimBuffer();
-
-  simSerial.println(F("AT+COPS?"));  // Check operator. See operator e.g Telia and if connected to NB or CAT-M
-  if (responseCheck("9", 2000) != 1) {
-    responseCheck("7", 2000);
-  }
-  clrsimBuffer();
-
-  simSerial.println(F("AT+CGNAPN"));  // Check if connected to the correct APN
-  responseCheck("\"iot.1nce.net\"", 2000);
-  clrsimBuffer();
-
-  simSerial.println(F("AT+CSQ"));  // Check signal quality
-  responseCheck("OK", 2000);
-  for (int i = 0; i < 2; i++) {  // Put response into IMEI array
-    CSQ[i] = simBuffer[i + 7];
-  }
-  clrsimBuffer();  // Clear sim response buffer
 
   simSerial.println(F("AT+CNACT=0,1"));  // Activate network
   responseCheck("0,ACTIVE", 2000);
@@ -398,78 +420,78 @@ void modbusRead() {
 
   if (!slaveSetupCheck) {  //Save information of slaves. Will only do after a setup.
 
-  unsigned short dataCount = 0;
-  char ID = 0;
-  short ADR = 0;
-  char VAR = 0;
-  short VAL = 0;
-  int IDCount = 0;
-  memset(IDarray, 0, sizeof(IDarray));
+    unsigned short dataCount = 0;
+    char ID = 0;
+    short ADR = 0;
+    char VAR = 0;
+    short VAL = 0;
+    int IDCount = 0;
+    memset(IDarray, 0, sizeof(IDarray));
 
-  dataCount = dataCount | simBuffer[2];  //Number of data/adresses to read
-  dataCount = dataCount << 8;
-  dataCount = dataCount | simBuffer[3];
+    dataCount = dataCount | simBuffer[2];  //Number of data/adresses to read
+    dataCount = dataCount << 8;
+    dataCount = dataCount | simBuffer[3];
 
-  for (int i = 0; i < dataCount; i++) {  //Save ID, adresses and initial value
-    ID = simBuffer[(i * 6) + 4];
-    ADR = simBuffer[(i * 6) + 5];
-    ADR = ADR << 8;
-    ADR |= simBuffer[(i * 6) + 6];
-    VAR = simBuffer[(i * 6) + 7];
-    VAL = simBuffer[(i * 6) + 8];
-    VAL = VAL << 8;
-    VAL |= simBuffer[(i * 6) + 9];
+    for (int i = 0; i < dataCount; i++) {  //Save ID, adresses and initial value
+      ID = simBuffer[(i * 6) + 4];
+      ADR = simBuffer[(i * 6) + 5];
+      ADR = ADR << 8;
+      ADR |= simBuffer[(i * 6) + 6];
+      VAR = simBuffer[(i * 6) + 7];
+      VAL = simBuffer[(i * 6) + 8];
+      VAL = VAL << 8;
+      VAL |= simBuffer[(i * 6) + 9];
 
-    EEPROM.put(ID, VAL);  //Put initial value into EEPROM
+      EEPROM.put(ID, VAL);  //Put initial value into EEPROM
 
-    if (!(strchr(IDarray, ID) > 0)) {  //If ID is not in IDarray, add to array and reset counters
-      IDarray[IDCount++] = ID;
-      slave[ID].digitalCount = 0;
-      slave[ID].analogCount = 0;
-      slave[ID].reset();
+      if (!(strchr(IDarray, ID) > 0)) {  //If ID is not in IDarray, add to array and reset counters
+        IDarray[IDCount++] = ID;
+        slave[ID].digitalCount = 0;
+        slave[ID].analogCount = 0;
+        slave[ID].reset();
+      }
+
+      if (VAR == 1) {  //If variable type is bool, add adress to digital adress
+        slave[ID].digitalAdress[slave[ID].digitalCount++] = ADR;
+
+      } else {  //Else add adress to analog adress
+        slave[ID].analogAdress[slave[ID].analogCount++] = ADR;
+      }
     }
 
-    if (VAR == 1) {  //If variable type is bool, add adress to digital adress
-      slave[ID].digitalAdress[slave[ID].digitalCount++] = ADR;
+    clrsimBuffer();  //Clear response buffer
 
-    } else {  //Else add adress to analog adress
-      slave[ID].analogAdress[slave[ID].analogCount++] = ADR;
-    }
-  }
+    for (int i = 0; i < strlen(IDarray); i++) {  //Go through IDarray
 
-  clrsimBuffer();  //Clear response buffer
+      if (slave[IDarray[i]].digitalCount > 0) {  //If there is digital inputs in slave
 
-  for (int i = 0; i < strlen(IDarray); i++) {  //Go through IDarray
+        slave[IDarray[i]].digitalSmallAdress = slave[IDarray[i]].digitalAdress[0];  //Assume first element is smallest
+        slave[IDarray[i]].digitalLargeAdress = slave[IDarray[i]].digitalAdress[0];  //Assume first element is smallest
 
-    if (slave[IDarray[i]].digitalCount > 0) {  //If there is digital inputs in slave
-
-      slave[IDarray[i]].digitalSmallAdress = slave[IDarray[i]].digitalAdress[0];  //Assume first element is smallest
-      slave[IDarray[i]].digitalLargeAdress = slave[IDarray[i]].digitalAdress[0];  //Assume first element is smallest
-
-      for (int j = 1; j < slave[IDarray[i]].digitalCount; j++) {
-        if (slave[IDarray[i]].digitalAdress[j] < slave[IDarray[i]].digitalSmallAdress) {  //Find the smallest digital adress
-          slave[IDarray[i]].digitalSmallAdress = slave[IDarray[i]].digitalAdress[j];
+        for (int j = 1; j < slave[IDarray[i]].digitalCount; j++) {
+          if (slave[IDarray[i]].digitalAdress[j] < slave[IDarray[i]].digitalSmallAdress) {  //Find the smallest digital adress
+            slave[IDarray[i]].digitalSmallAdress = slave[IDarray[i]].digitalAdress[j];
+          }
+          if (slave[IDarray[i]].digitalAdress[j] > slave[IDarray[i]].digitalLargeAdress) {  //Find the largest digital adress
+            slave[IDarray[i]].digitalLargeAdress = slave[IDarray[i]].digitalAdress[j];
+          }
         }
-        if (slave[IDarray[i]].digitalAdress[j] > slave[IDarray[i]].digitalLargeAdress) {  //Find the largest digital adress
-          slave[IDarray[i]].digitalLargeAdress = slave[IDarray[i]].digitalAdress[j];
+      }
+      if (slave[IDarray[i]].analogCount > 0) {                                    //If there is digital inputs in slave
+        slave[IDarray[i]].analogSmallAdress = slave[IDarray[i]].analogAdress[0];  //Assume first element is smallest
+        slave[IDarray[i]].analogLargeAdress = slave[IDarray[i]].analogAdress[0];  //Assume first element is largest
+
+        for (int j = 1; j < slave[IDarray[i]].analogCount; j++) {
+          if (slave[IDarray[i]].analogAdress[j] < slave[IDarray[i]].analogSmallAdress) {  //Find the smallest analog adress
+            slave[IDarray[i]].analogSmallAdress = slave[IDarray[i]].analogAdress[j];
+          }
+          if (slave[IDarray[i]].analogAdress[j] > slave[IDarray[i]].analogLargeAdress) {  //Find the largest analog adress
+            slave[IDarray[i]].analogLargeAdress = slave[IDarray[i]].analogAdress[j];
+          }
         }
       }
     }
-    if (slave[IDarray[i]].analogCount > 0) {                                    //If there is digital inputs in slave
-      slave[IDarray[i]].analogSmallAdress = slave[IDarray[i]].analogAdress[0];  //Assume first element is smallest
-      slave[IDarray[i]].analogLargeAdress = slave[IDarray[i]].analogAdress[0];  //Assume first element is largest
-
-      for (int j = 1; j < slave[IDarray[i]].analogCount; j++) {
-        if (slave[IDarray[i]].analogAdress[j] < slave[IDarray[i]].analogSmallAdress) {  //Find the smallest analog adress
-          slave[IDarray[i]].analogSmallAdress = slave[IDarray[i]].analogAdress[j];
-        }
-        if (slave[IDarray[i]].analogAdress[j] > slave[IDarray[i]].analogLargeAdress) {  //Find the largest analog adress
-          slave[IDarray[i]].analogLargeAdress = slave[IDarray[i]].analogAdress[j];
-        }
-      }
-    }
   }
-}
   slaveSetupCheck = 1;  //Setup is done, will only run again if setup is recieved from server.
 
   //Define ADU size
@@ -607,7 +629,6 @@ void modbusRead() {
 
   state = serversend;  //Change state to send message
   // millisDelay(7000);
-
 }
 
 void sendToServer() {
@@ -615,28 +636,28 @@ void sendToServer() {
   msgPos2 = 12;
   char serverMessage[500];
   memset(serverMessage, 0, sizeof serverMessage);
-  
+
   if (totalAdress > 0) {  //If there are adress data to be send
 
     for (int i = 0; i < strlen(IMEI); i++) {  //Add IMEI to message
       serverMessage[i] = IMEI[i];
     }
 
-  int num = totalAdress;
+    int num = totalAdress;
 
     if (totalAdress < 98) {
-      for (int j = 0; j < (num*5); j++) {
+      for (int j = 0; j < (num * 5); j++) {
         serverMessage[msgPos2++] = messageBuffer[headPos++];
         totalAdress = 0;
-        if(headPos > 1994){
+        if (headPos > 1994) {
           headPos = 0;
         }
       }
-    } else{
+    } else {
       for (int j = 0; j < 485; j++) {
         serverMessage[msgPos2++] = messageBuffer[headPos++];
         totalAdress = totalAdress - 97;
-        if(headPos > 1994){
+        if (headPos > 1994) {
           headPos = 0;
         }
       }
@@ -675,7 +696,6 @@ void sendToServer() {
     serverMessage[msgPos2] = highByte(CRC2);  //Put CRC into message
     msgPos2++;
     serverMessage[msgPos2] = lowByte(CRC2);
-
   }
 
   char sendLength[20] = "AT+CASEND=0,";  // To hold at command that sends size of data to be send
@@ -692,14 +712,6 @@ void sendToServer() {
   simSerial.write(serverMessage, msgPos2 + 1);  // Send message to server
 
   millisDelay(300);
-
-  simSerial.println(F("AT+CACLOSE=0"));  // Check if connected to the correct APN
-  responseCheck("OK", 300);
-  clrsimBuffer();
-
-  simSerial.println(F("AT+CAOPEN=0,0,\"UDP\",\"164.92.164.168\",8080"));  // Open UDP connection/socket
-  responseCheck("OK", 2000);
-  clrsimBuffer();
 }
 
 void modbusWrite() {  //Write data to slaves
